be507f521613a261ec748211b0d032cf839a3618
haxwithaxe <me@haxwithaxe.net>
2017-04-30 21:09:42 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fakeanese',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1.0',
+
+    description='',
+    long_description='',
+
+    # The project's main homepage.
+    url='',
+
+    # Author details
+    author='',
+    author_email='',
+
+    # Choose your license
+    license='GPLv3',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+)
